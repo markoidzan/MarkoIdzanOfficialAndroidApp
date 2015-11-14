9c63dd7d584ec28d327df2ba01ba378d951375b7
Marko Idzan <midzan@tvz.hr>
2015-11-14 15:12:27 +0100

    First upload
    Upload #1
    * Basic nav drawer is made
    * All classes which would load WebView component is ready
    * More coming soon

---
+++ app/src/main/java/markoidzan/markoidzanofficial/FirstScreen.java
@@ -0,0 +1,7 @@
+package markoidzan.markoidzanofficial;
+
+/**
+ * Created by midza on 14.11.2015..
+ */
+public class FirstScreen {
+}
---
+++ app/src/main/java/markoidzan/markoidzanofficial/MainActivity.java
@@ -0,0 +1,122 @@
+package markoidzan.markoidzanofficial;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Created by midza on 13.11.2015..
+ */
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = setupDrawerToggle();
+
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+
+        setupDrawerContent(nvDrawer);
+
+        mDrawer.setDrawerListener(drawerToggle);
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem (MenuItem menuItem) {
+        Fragment fragment = null;
+
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_firstscreen:
+                fragmentClass = FirstScreen.class;
+                break;
+            case R.id.nav_youtube:
+                fragmentClass = YouTube.class;
+                break;
+            case R.id.nav_facebook:
+                fragmentClass = Facebook.class;
+                break;
+            case R.id.nav_twitter:
+                fragmentClass = Twitter.class;
+                break;
+            case R.id.nav_instagram:
+                fragmentClass = Instagram.class;
+                break;
+            case R.id.nav_website:
+                fragmentClass = WebSite.class;
+                break;
+            default:
+                fragmentClass = YouTube.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawer.closeDrawers();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+}
